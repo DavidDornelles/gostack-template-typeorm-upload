9b24103a96b8381aeab3b0879329ab0dee4c8bea
DavidDornelles <daviddornelles@protonmail.com>
2021-04-26 02:37:39 -0300

    list transactions, create transactions+category

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,60 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    type,
+    value,
+    category,
+  }: RequestDTO): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository); // custom methods of repository [ getBalance() ]
+    const categoriesRepository = getRepository(Category); // native methods
+
+    const { total } = await transactionsRepository.getBalance();
+    const result = total - value;
+    if (type === 'outcome' && result < 0) {
+      throw new AppError(
+        'You cannot create a outcome transaction without a valid balance.',
+      );
+    }
+
+    let transactionCategory = await categoriesRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(transactionCategory);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      type,
+      value,
+      category: transactionCategory,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
